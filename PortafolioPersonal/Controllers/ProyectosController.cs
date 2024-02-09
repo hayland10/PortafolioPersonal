b6dc8983b1cc936f8b75e706d9c03a918dc24f5c
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-02-09 00:34:11 -0500

    proyectos css y model

---
+++ PortafolioPersonal/Controllers/ProyectosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortafolioPersonal.Controllers
+{
+    public class ProyectosController : Controller
+    {
+        public ActionResult Proye() => View();
+
+
+     }
+}
---
+++ PortafolioPersonal/Models/Proyecto.cs
@@ -0,0 +1,9 @@
+﻿namespace PortafolioPersonal.Models
+{
+    public class Proyecto
+    {
+        public string Titulo { get; set; }
+        public string ImagenUrl { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
