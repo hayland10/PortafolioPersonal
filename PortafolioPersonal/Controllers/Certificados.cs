087d812405871aa3c1349477740efeebe855f81f
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-03-07 20:09:51 -0500

    Mejoras ccs, add certififcados y swipper

---
+++ PortafolioPersonal/Controllers/Certificados.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortafolioPersonal.Controllers
+{
+    public class Certificados : Controller
+    {
+        
+        public ActionResult Certi()
+        {
+            return View();
+        }
+
+    }
+}
