5b0cc963bb2e2d853305aa09c2d44022c723db06
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-03-11 22:20:34 -0500

    Proyecto terminado

---
+++ PortafolioPersonal/Controllers/softSkills.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortafolioPersonal.Controllers
+{
+    public class softSkills : Controller
+    {
+        // GET: softSkills
+        public ActionResult sSkills() => View();
+        
+
+        
+    }
+}
